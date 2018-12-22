85076112beb9a8d221a7a9ab8db497bae1943541
Vince Andrew Pimentel <contact@vincep.me>
2018-12-22 00:53:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,11 +81,11 @@ class Student
     self.all_students_in_grade_X(12, "<")
   end
 
-  def self.first_X_students_in_grade_10(number, grade, operator = "=")
+  def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
       SELECT #{number}
       FROM students
-      WHERE grade #{operator} ?;
+      WHERE grade = ?;
     SQL
 
     DB[:conn].execute(sql, grade).map do |row|
